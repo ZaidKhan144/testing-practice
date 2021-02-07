f22970c2540b8d3b333b10291d28b1f08812b7e7
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-02-07 16:14:42 -0600

    ceasar cipher logic

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -6,7 +6,10 @@ test('should be able to check whether caesarCipher is defined or not', () => {
     expect(caesarCipher).toBeDefined()
 })
 
-test('should be able to implement ceasar cipher logic', () => {
+test('should be able to check lowercase ceasar cipher logic', () => {
     expect(caesarCipher('defend the east wall of the castle', 1)).toEqual('efgfoe uif fbtu xbmm pg uif dbtumf')
 })
 
+test('should be able to check uppercase ceasar cipher logic', () => {
+    expect(caesarCipher('ATTACK AT DAWN', 1)).toEqual('BUUBDL BU EBXO')
+})
